61d5354244f17e8ccb25f8662793cb1aca00ecd8
Ernesto Alejo <ernestokarim@gmail.com>
2016-11-16 12:15:23 +0100

    Improve comment.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 
-// Think this was replaced by a tool everywhere, not manually written in the app.
+// Think the name of the package was replaced by a tool everywhere, not manually written in the app,
+// so there is no problem with two Vues imported at runtime.
 import Vue from 'vue/dist/vue.js';
